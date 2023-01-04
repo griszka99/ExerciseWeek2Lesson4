47fbe9f3eac7ec604d1cc07dd9d509a12f51170c
grzeg <grzeg@GRZEGORZ>
2023-01-04 23:43:56 +0100

    Add project files.

---
+++ ExerciseWeek2/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exercise Week 2...");
+
+//Ex1:
+
+string name;
+string lastname;
+int wiek;
+char sex;
+long personalId;
+short emploId;
+
+
+//Ex2:
+
+char a = 'a';
+char b = 'b';
+char c = 'c';
+Console.WriteLine($"{c}{b}{a}");
+
+//Ex3:
+
+int x =  2;
+int y = 3;
+double z = Math.Sqrt(Math.Pow(x,2)+ Math.Pow(y,2));
+Console.WriteLine($"{z}");
+
+//Ex4:
+
+int ten = 10;
+Console.WriteLine($"{ten}");
+string text = "Dotnet school";
+Console.WriteLine($"{text}");
+double decimalNumber = 12.5;
+Console.WriteLine($"{decimalNumber}");
+
+//Ex5 in other file...
+
+
+
+
+
+
+
+
+
+
+
+
+
